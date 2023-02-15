94f1a2fe7ef1c99f7b54e562018ee3d59b7412a7
RigHakiko <add-122@hotmail.com>
2023-02-15 18:02:48 +0900

    写完了第2课的一部分前端, 要运行了

---
+++ 20230208_01_project_vue2_cli_01/src/router/store.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+
+    state: {
+        CollateralProviderZhyInContent02: {
+            CpTanpoMono: "",
+            CpNameMei: "",
+            CpNameMeiKana: "",
+            CpNameSei: "",
+            CpNameSeiKana: "",
+            CpBirthDateYear: "",
+            CpBirthDate: null,
+            CpMoushikomi: "",
+            CpRentaiSaimushaToOnaji: "",
+            CpPhone: "",
+            CpHonninShokugyoCode: "",
+            CpHonninKinmusakiName: "",
+            CpReason: "",
+            CpSame: ""
+        }
+    }
+});
+
+export default store;
---
+++ 20230208_01_project_vue2_cli_01/src/store/index.js
@@ -0,0 +1,12 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+
+Vue.use(VueRouter);
+
+import Content1 from '../components/Content1'
+import Content2 from '../components/Content2'
+const routes = [
+    {
+        path: '/'
+    }
+]
