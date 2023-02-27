9c591608d1ea5430944030265f165d95eaea7ad4
RigHakiko <add-122@hotmail.com>
2023-02-27 16:08:35 +0900

    加了content03, 并加入之到vue-router中了

--- 20230208_01_project_vue2_cli_01/src/router/index.js
+++ 20230208_01_project_vue2_cli_01/src/router/index.js
@@ -5,6 +5,7 @@ Vue.use(VueRouter);
 
 import Content01 from '../components/Content01'
 import Content02 from '../components/Content02'
+import Content03 from '../components/Content03'
 const routes = [
     {
         path: '/Content02',
@@ -15,6 +16,11 @@ const routes = [
         path: '/',
         name: 'Content01',
         component: Content01
+    },
+    {
+        path: '/Content03',
+        name: 'Content03',
+        component: Content03
     }
 ]
 
