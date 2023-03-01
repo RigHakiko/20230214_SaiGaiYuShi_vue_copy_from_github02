0b418adee609c89c230367041d8e8f6b5eb69c99
RigHakiko <add-122@hotmail.com>
2023-03-01 11:03:09 +0900

    部分弄了都道府县的问题, 给bKariireGakuNenHensai12No1前面加上d, dbHisaiShinzokuDokyoAri加上value, buttons加上显示, 都道府县补0, 遇到了单个checkbox存非boolean值的问题

--- 20230208_01_project_vue2_cli_01/src/components/Content03.vue
+++ 20230208_01_project_vue2_cli_01/src/components/Content03.vue
@@ -53,7 +53,7 @@
             <div>
                 <label class="description">被災住宅の修理が不能又は困難である</label>
                 <div class="kiho">
-                    <input type="checkbox" value="困難である" v-model="buildingZhy.dbHigaiJokyoDaikiboHankaiOrHankai" />困難である
+                    <input type="checkbox"  v-model="buildingZhy.dbHigaiJokyoDaikiboHankaiOrHankai" value="困難である" />困難である
                 </div>
             </div>
 
@@ -74,10 +74,10 @@
                 <label class="description">物件_敷地（所在地）_都道府県</label>
                 <div class="kiho">
                     <select v-model="buildingZhy.aplBukkenAddressTodofuken">
+                        <option :value="index" v-for="(option, index) in prefectures">{{ index }}, {{ option }}</option>
+                        <!-- <option>{{ index }}</option>
                         <option></option>
-                        <option></option>
-                        <option></option>
-                        <option></option>
+                        <option></option> -->
                     </select>
                 </div>
             </div>
@@ -427,7 +427,7 @@
             <div>
                 <label class="description">借入希望額_年間返済額の1/12</label>
                 <div class="kiho">
-                    <input type="number" v-model="buildingZhy.bKariireGakuNenHensai12No1"  />
+                    <input type="number" v-model="buildingZhy.dbKariireGakuNenHensai12No1"  />
                 </div>
             </div>
             <div>
@@ -443,8 +443,8 @@
                 <label class="description">被災親族同居の有無</label>
                 <div class="kiho">
                     <select v-model="buildingZhy.dbHisaiShinzokuDokyoAri">
-                        <option value="">01有</option>
-                        <option value="">02無</option>
+                        <option value="01有">01有</option>
+                        <option value="02無">02無</option>
                     </select>
                 </div>
             </div>
@@ -513,8 +513,8 @@
             </div>
         </div>
         <!-- <div>{{ num }}</div> -->
-        <button @click="tmp()"></button>
-        <button @click="next()"></button>
+        <button @click="tmp()">tmp</button>
+        <button @click="next()">next</button>
     </div>
 </template>
 <script>
@@ -579,7 +579,7 @@ export default {
                 dbKariireGakuKingaku: '',
                 dbKariireGakuKinri: '',
                 dbKariireGakuHensaiKikan: '',
-                bKariireGakuNenHensai12No1: '',
+                dbKariireGakuNenHensai12No1: '',
                 dbHensaiHouhou: '',
                 dbHisaiShinzokuDokyoAri: '',
                 dbSueokiKikanNoNensu: '',
@@ -613,7 +613,7 @@ export default {
             // flag
 
 
-            prefectures: {"1":"北海道","2":"青森県","3":"岩手県","4":"宮城県","5":"秋田県","6":"山形県","7":"福島県","8":"茨城県","9":"栃木県","10":"群馬県","11":"埼玉県","12":"千葉県","13":"東京都","14":"神奈川県","15":"新潟県","16":"富山県","17":"石川県","18":"福井県","19":"山梨県","20":"長野県","21":"岐阜県","22":"静岡県","23":"愛知県","24":"三重県","25":"滋賀県","26":"京都府","27":"大阪府","28":"兵庫県","29":"奈良県","30":"和歌山県","31":"鳥取県","32":"島根県","33":"岡山県","34":"広島県","35":"山口県","36":"徳島県","37":"香川県","38":"愛媛県","39":"高知県","40":"福岡県","41":"佐賀県","42":"長崎県","43":"熊本県","44":"大分県","45":"宮崎県","46":"鹿児島県","47":"沖縄県"}
+            prefectures: {"01":"北海道","02":"青森県","03":"岩手県","04":"宮城県","05":"秋田県","06":"山形県","07":"福島県","08":"茨城県","09":"栃木県","10":"群馬県","11":"埼玉県","12":"千葉県","13":"東京都","14":"神奈川県","15":"新潟県","16":"富山県","17":"石川県","18":"福井県","19":"山梨県","20":"長野県","21":"岐阜県","22":"静岡県","23":"愛知県","24":"三重県","25":"滋賀県","26":"京都府","27":"大阪府","28":"兵庫県","29":"奈良県","30":"和歌山県","31":"鳥取県","32":"島根県","33":"岡山県","34":"広島県","35":"山口県","36":"徳島県","37":"香川県","38":"愛媛県","39":"高知県","40":"福岡県","41":"佐賀県","42":"長崎県","43":"熊本県","44":"大分県","45":"宮崎県","46":"鹿児島県","47":"沖縄県"}
         }
     },
     methods: {
